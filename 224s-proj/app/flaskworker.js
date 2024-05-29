6281a79cdd0a26dc9aeef427b468b8b93f1d8325
Michelle Fu <mechllfu@gmail.com>
2024-05-29 12:37:52 -0700

    can pass recorded audio but transcription is bad

--- 224s-proj/app/flaskworker.js
+++ 224s-proj/app/flaskworker.js
@@ -35,27 +35,27 @@ async function sendRequest(audioData) {
 self.addEventListener('message', async (event) => {
     // Load audio data
     // let url = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/jfk.wav'; // replace this later
-    let url = event.url
+    let url = event.data.url
     let buffer = Buffer.from(await fetch(url).then(x => x.arrayBuffer()))
     console.log(buffer)
     // Read .wav file and convert it to required format
-    // let wav = new WaveFile(buffer);
-    // wav.toBitDepth('32f'); // Pipeline expects input as a Float32Array
-    // wav.toSampleRate(16000); // Whisper expects audio with a sampling rate of 16000
-    // let audioData = wav.getSamples();
-    // if (Array.isArray(audioData)) {
-    //     if (audioData.length > 1) {
-    //         const SCALING_FACTOR = Math.sqrt(2);
+    let wav = new WaveFile(buffer);
+    wav.toBitDepth('32f'); // Pipeline expects input as a Float32Array
+    wav.toSampleRate(16000); // Whisper expects audio with a sampling rate of 16000
+    let audioData = wav.getSamples();
+    if (Array.isArray(audioData)) {
+        if (audioData.length > 1) {
+            const SCALING_FACTOR = Math.sqrt(2);
 
-    //         // Merge channels (into first channel to save memory)
-    //         for (let i = 0; i < audioData[0].length; ++i) {
-    //         audioData[0][i] = SCALING_FACTOR * (audioData[0][i] + audioData[1][i]) / 2;
-    //         }
-    //     }
-    // }
-    // // Select first channel
-    // audioData = Array.from(audioData[0]);
-    let audioData = Array.from(buffer);
+            // Merge channels (into first channel to save memory)
+            for (let i = 0; i < audioData[0].length; ++i) {
+            audioData[0][i] = SCALING_FACTOR * (audioData[0][i] + audioData[1][i]) / 2;
+            }
+        }
+    }
+    // Select first channel
+    audioData = Array.from(audioData[0]);
+    // let audioData = Array.from(buffer);
     console.log(audioData)
     sendRequest(audioData);
 });
