24c3ce2013e77c58dd6d7f5013dad97ff4bfb544
Zhang Zhiyuan <zhiyuan102@gmail.com>
2024-06-02 16:16:33 -0700

    blah

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, make_response
 from transformers import pipeline
 import numpy as np
+from openai import OpenAI
+import json
 
 app = Flask(__name__)
 
@@ -18,8 +20,38 @@ def transcribe_audio():
             audio_np = np.array(data, dtype=np.float32)
 
             transcription = transcriber(audio_np)
-            print(transcription)
-            response = make_response(jsonify({"transcription": transcription['text']}))
+
+            try:
+                client = OpenAI()
+
+                response = client.chat.completions.create(
+                    model="gpt-4o",
+                    response_format={ "type": "json_object" },
+                    messages=[
+                        {"role": "system", "content": "You are a helpful translater designed to output JSON."},
+                        {"role": "user", "content": f"""Translate the following text to both English and Chinese:
+                         {transcription}
+
+                        Use the following format:
+                        English: <english translation>
+                        Chinese: <chinese translation>
+                         """}
+                    ]
+                )
+                print(response)
+                eng_translation = json.loads(response.choices[0].message.content)["English"].strip()
+                chi_translation = json.loads(response.choices[0].message.content)["Chinese"].strip()
+
+            except Exception as e:
+                print(e)
+                eng_translation = "NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
+                chi_translation = "NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
+
+            response = make_response(jsonify({
+                "transcription": transcription['text'], 
+                "eng_translation": eng_translation,
+                "chi_translation": chi_translation
+                }))
             response.headers['Access-Control-Allow-Origin'] = '*'
             response.headers['Access-Control-Allow-Methods'] = 'POST,PATCH,OPTIONS'
             return response
